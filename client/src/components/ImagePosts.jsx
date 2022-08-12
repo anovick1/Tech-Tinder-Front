97c7f1ebf06a2976c068d0b9e0a0c3a2b1a571d2
Jason Martinez <jasonmar08@gmail.com>
2022-08-12 11:23:47 -0400

    styling on image post card

--- client/src/components/ImagePosts.jsx
+++ client/src/components/ImagePosts.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const ImagePosts = () => {
-  return <div>ImagePosts</div>
+const ImagePosts = ({post, displayedUser}) => {
+  return (
+    <div className="image-post">
+      <div className="post-image">
+        <img src={post.img_url} alt="image post" />
+      </div>
+      <div className="image-text">
+        <h4>{post.caption}</h4>
+      </div>
+    </div>
+  )
 }
 
 export default ImagePosts
