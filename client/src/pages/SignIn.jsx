c643849914b9184ebe5ec1609d8b69b60c4a6de8
MatthewScott <mattwillscott95@gmail.com>
2022-08-14 20:45:51 -0600

    CSS on the sign in page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,9 +4,19 @@ import SignInForm from '../components/SignInForm'
 
 const SignIn = ({ setCurrentUser }) => {
   return (
-    <div>
+    
+    <div className= "login-page">
+      <div className= 'intro-words'>
+        <h2>Welcome to</h2>
+        <h1>Tech-Tinder</h1>
+        <h3>The place to meet fellow nerds and maybe find your soulmate</h3>
+     </div>
+     <div>
       <RegisterForm />
+      </div>
+      <div>
       <SignInForm setCurrentUser={setCurrentUser} />
+      </div>
     </div>
   )
 }
