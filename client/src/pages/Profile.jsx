ecc4dfd872b602bce93b2057730b7b22d2eb8ae7
MatthewScott <mattwillscott95@gmail.com>
2022-08-15 14:41:17 -0600

    posts are showing up in timestamped order BABY

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 import ShowProfileFeed from '../components/ShowProfileFeed'
 import ProfileEditForm from '../components/EditProfile'
 
-const Profile = ({ currentUser, posts, users, setCurrentUser }) => {
+const Profile = ({ currentUser, posts, users, setCurrentUser, setPosts }) => {
   const [edit, setEdit] = useState(false)
   const displayedUser = currentUser
 
@@ -27,6 +27,7 @@ const Profile = ({ currentUser, posts, users, setCurrentUser }) => {
         edit={edit}
         setEdit={setEdit}
         setCurrentUser={setCurrentUser}
+        setPosts={setPosts}
       />
     )
   ) : (
